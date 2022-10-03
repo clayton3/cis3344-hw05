ad0ff80cda3c31d0408efff8115dc5f251fe99ab
clayton3 <claytoncblevins@gmail.com>
2022-10-03 16:54:08 -0400

    Updated slidShowContent, created food.json and made a .gitignore to remove .DS_Store

--- js/content/slideShowContent.js
+++ js/content/slideShowContent.js
@@ -1,11 +1,23 @@
 function slideShowContent(){
-    var content = `
+    const content = `
         <h2>Slide Show Page</h2>
     `;
 
-    var ele = document.createElement("div");
-    ele.classList.add("slideShow")
+    const slideShowContainer = document.createElement("div");
+
+    ajax("json/.json", processCatList, document.getElementById("content"));
+    function processCatList(catList){
+        for(var i = 0; i < catList.length; i++)
+            catList[i].caption = catList[i].nickname;
+
+        console.log(catList[0].pic + " | " + catList[0].caption);
+        var ss = MakeSlideShow(catList, "slideShow");
+        content.appendChild(ss);
+    }
+
+    const ele = document.createElement("div");
     ele.innerHTML = content;
+    ele.appendChild(slideShowContainer)
 
     return ele;
 }
